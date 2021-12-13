c6cae624614388b6280682bd02dfb48c10872539
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-12-13 20:33:44 -0300

    implemented feature management with appsettings

---
+++ src/FeatureFlag.API/Controllers/PaymentsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+using System.Threading.Tasks;
+
+namespace FeatureFlag.API.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public PaymentsController(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        [HttpGet("health-check")]
+        public async Task<IActionResult> CheckFlags()
+        {
+            var flagsState = new
+            {
+                EnabledCreditCardPayments = await _featureManager.IsEnabledAsync("CreditCardPayments"),
+                EnabledDebitCardPayments = await _featureManager.IsEnabledAsync("DebitCardPayments"),
+            };
+
+            return Json(flagsState);
+        }
+
+        [HttpPost("credit-payment")]
+        public async Task<IActionResult> CreditPayment()
+        {            
+            if (await _featureManager.IsEnabledAsync("CreditCardPayments"))
+            {
+                return Ok("Successful credit payment");
+            }
+
+            return BadRequest("This functionality is disabled");
+        }
+
+        [HttpPost("debit-payment")]
+        public async Task<IActionResult> DebitPayment()
+        {
+            if (await _featureManager.IsEnabledAsync("DebitCardPayments"))
+            {
+                return Ok("Successful debit payment");
+            }
+
+            return BadRequest("This functionality is disabled");
+        }
+    }
+}
--- src/FeatureFlag.API/Startup.cs
+++ src/FeatureFlag.API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,19 +24,27 @@ namespace FeatureFlag.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Feature flag API", Version = "v1" });
+            });
+
+            services.AddFeatureManagement();
+        }
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo.API v1"));
 
             app.UseHttpsRedirection();
 
